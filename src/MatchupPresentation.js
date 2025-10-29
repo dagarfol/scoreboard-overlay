fc2b4c474ebf9710d5d95418e99090ca69da7d13
david-garcia-folguera <david.garcia-folguera@allianzdirect.es>
2025-10-29 16:01:14 +0100

    center matchup on screen

--- src/MatchupPresentation.js
+++ src/MatchupPresentation.js
@@ -6,26 +6,28 @@ const MatchupPresentation = ({ matchData }) => {
   const { teamA, teamB, competition, competitionLogo, category, location } = matchData;
 
   return (
-    <div className={styles['matchup-card']}>
-      <div className={styles['details-container']}>
-        <div className={styles['competition-info']}>
-          <span className={styles['competition']}>{competition}</span>
+    <div className={`${styles['matchup-wrapper']} ${styles['centered']}`}>
+      <div className={styles['matchup-card']}>
+        <div className={styles['details-container']}>
+          <div className={styles['competition-info']}>
+            <span className={styles['competition']}>{competition}</span>
+          </div>
+          <span className={styles['category']}>{category}</span>
+          <span className={styles['location']}>{location}</span>
         </div>
-        <span className={styles['category']}>{category}</span>
-        <span className={styles['location']}>{location}</span>
-      </div>
-      <div className={styles['teams-container']}>
-        <div className={styles['team']}>
-          <img src={teamA.logo} alt={teamA.name} className={styles['team-logo']} />
-          <span className={styles['team-name']}>{teamA.name}</span>
-        </div>
-        <div className={styles['vs-container']}>
-          <span className={styles['vs']}>vs</span>
-          {competitionLogo && <img src={competitionLogo} alt="Competition Logo" className={styles['competition-logo']} style={{backgroundColor: '#bdc3c7', padding: '5px'}}/>}
-        </div>
-        <div className={styles['team']}>
-          <img src={teamB.logo} alt={teamB.name} className={styles['team-logo']} />
-          <span className={styles['team-name']}>{teamB.name}</span>
+        <div className={styles['teams-container']}>
+          <div className={styles['team']}>
+            <img src={teamA.logo} alt={teamA.name} className={styles['team-logo']} />
+            <span className={styles['team-name']}>{teamA.name}</span>
+          </div>
+          <div className={styles['vs-container']}>
+            <span className={styles['vs']}>vs</span>
+            {competitionLogo && <img src={competitionLogo} alt="Competition Logo" className={styles['competition-logo']} style={{ backgroundColor: '#bdc3c7', padding: '5px' }} />}
+          </div>
+          <div className={styles['team']}>
+            <img src={teamB.logo} alt={teamB.name} className={styles['team-logo']} />
+            <span className={styles['team-name']}>{teamB.name}</span>
+          </div>
         </div>
       </div>
     </div>
